723e19b612c35c81bb4cc046cfc25ca3ee5c3939
peacekeeper <markus@projectdanube.org>
2015-03-04 16:45:03 +0100

    Changed storage directory, Added Apache configuration, Allow sign-up for storage

--- debian/server.js
+++ debian/server.js
@@ -1,11 +1,12 @@
 process.umask(077);
 
 var reStore = require('restore'),
-    store   = new reStore.FileTree({path: 'storage'}),
+    store   = new reStore.FileTree({path: '/var/lib/nodejs/restore/storage'}),
 
     server  = new reStore({
                 store:  store,
-                http:   {port: process.env.PORT}
+                http:   {port: process.env.PORT},
+                allow:  {signup:true}
               });
 
 server.boot();
